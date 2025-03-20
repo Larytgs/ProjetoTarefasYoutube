86c0147ebcf1060eef96238a740fdc454a0030bf
laryTgs <laryssatortato99@gmail.com>
2025-03-20 19:02:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Deixe vazio para evitar que módulos sejam externalizados
 })
