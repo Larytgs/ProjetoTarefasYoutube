7e95e18d6ffdf78b9af1e0200095949d6779be02
laryTgs <laryssatortato99@gmail.com>
2025-03-21 14:20:21 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "react-router-dom": new URL(
-        "node_modules/react-router-dom/",
-        import.meta.url
-      ).pathname,
-    },
-  },
+export default defineConfig(
+  {
+    plugins: [react()],
+  }
   // build: {
   //   rollupOptions: {
   //     external: ["react-router-dom"], // Deixe vazio para evitar que módulos sejam externalizados
   //   },
   // },
-});
+);
